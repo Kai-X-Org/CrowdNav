4a87e34c14e8285061c6f2716d1c5c63fad03c9d
Kai-X-Org <k.px@berkeley.edu>
2025-03-30 23:59:10 -0700

    fixed arguments to run 16 processes. in previous commit forgot to mention also fixed num humans to be 4

--- arguments.py
+++ arguments.py
@@ -51,7 +51,7 @@ def get_args():
     parser.add_argument(
         '--num-processes',
         type=int,
-        default=8,
+        default=16,
         help='how many training processes to use (default: 16)')
 
     parser.add_argument(
